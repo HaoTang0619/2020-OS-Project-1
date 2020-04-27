e563223e0e3e9cf465f61b1e234952f482b47dea
HaoTang0619 <b06902020@ntu.edu.tw>
2020-04-27 12:45:46 +0800

    Basic files

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # 2020-OS-Project-1
-Spec: https://hackmd.io/@Ue96nvjESj2XsDXw532-qA/ryYqceUrU
+
+** B06902020 唐浩 **
+
+- Spec: https://hackmd.io/@Ue96nvjESj2XsDXw532-qA/ryYqceUrU
+
+## Environment
+- OS: Ubuntu 16.04
+- GCC version: 5.4.0
+- Python version: 3.5.2 (For running all input files, not necessary.)
+
+## Usage
+```powershell
+# Compile
+make
+
+# Execute ("sudo" is for outputing to dmesg.)
+## Single case:
+## This will generate outputs to stdout and dmesg.
+sudo ./make < <INPUT FILE>
+
+## All input files:
+python3 run_all_input.py <INPUT FILE DIRECTORY>
+```
+
+## Remarks
+- Start time: 紀錄子行程被第一次丟上cpu執行時的時間。
+- No need to compile the kernel.
---
+++ main.c
@@ -0,0 +1,230 @@
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<time.h>
+
+#define __USE_GNU
+#include<sched.h>
+
+#define SIGUSR3 SIGXFSZ
+#include<signal.h>
+
+typedef struct{
+    char name[64]; 
+    int R, T;
+    // For: take turns, burst counts, check RR, check 1st-time, check running:
+    int turn, cnt, robin, first, running;
+    pid_t pids;
+}Process;
+
+Process *proc; // Information of each child.
+char policy[10]; 
+int N, arrived = -1, rounder;
+
+void Unit(){
+    volatile unsigned long i;
+    for(i = 0; i < 1000000UL; i++);
+    return;
+}
+
+static void sig_usr(int signo){
+    // A child exit / pause.
+    if(signo == SIGUSR1){
+        /* DO NOT DECLARE VARIABLES IN A SWITCH STATEMENT */
+        int i; // Iterator
+        int min_T = 1e9, min_idx, remain; // SJF && PSJF
+        switch(policy[0]){
+            // FIFO & RR
+            case 'F': case 'R':
+                // Round each process.
+                for(i = 0; i <= arrived + 1; i++){
+                    if(proc[rounder].cnt < proc[rounder].T){
+                        if(proc[rounder].robin == 0){
+                            while(proc[rounder].running == 0) 
+                                kill(proc[rounder].pids, SIGUSR2);
+                            break;
+                        }
+                        else proc[rounder].robin = 0;
+                    }
+                    rounder = (rounder + 1) % (arrived + 1);
+                }
+                break;
+            
+            // SJF & PSJF
+            case 'S': case 'P':
+                for(i = 0; i <= arrived; i++){
+                    if(proc[i].cnt < proc[i].T){
+                        remain = proc[i].T - proc[i].cnt;
+                        if(min_T > remain){
+                            min_T = remain;
+                            min_idx = i;
+                        }
+                    }
+                }
+                if(min_T != 1e9) 
+                    while(proc[min_idx].running == 0)
+                        kill(proc[min_idx].pids, SIGUSR2);
+                break;
+        }
+    }
+    // Wake a child to run.
+    else if(signo == SIGUSR2) proc[arrived].turn = 1;
+    // Let a child pause.
+    else if(signo == SIGUSR3) proc[arrived].turn = 0;
+    return;
+}
+
+void PRINT_TO_DMESG(struct timespec start_time, struct timespec end_time){
+    FILE *fptr = fopen("/dev/kmsg", "a");
+    fprintf(fptr, "[Project1] %d %ld.%09ld %ld.%09ld\n", getpid(), start_time.tv_sec, start_time.tv_nsec, end_time.tv_sec, end_time.tv_nsec);
+    fclose(fptr);
+    return;
+}
+
+int compare(const void *a, const void *b){
+    Process *A = (Process*)a;
+    Process *B = (Process*)b;
+    if(A->R != B->R) return (A->R - B->R);
+    else{
+        switch(policy[0]){
+            // FIFO & RR
+            case 'F': case 'R':
+                return (atoi(&(A->name[1])) - atoi(&(B->name[1])));
+
+            // SJF & PSJF
+            case 'S': case 'P':
+                return (A->T - B->T);
+        }
+    }
+}
+
+int main(){
+    // Affinity (0)
+    cpu_set_t mask;
+    CPU_ZERO(&mask);
+    CPU_SET(0, &mask); 
+    sched_setaffinity(0, sizeof(mask), &mask);
+
+    // Signal
+    signal(SIGUSR1, sig_usr);
+    signal(SIGUSR2, sig_usr);
+    signal(SIGUSR3, sig_usr);
+
+    // Create shared memory.
+    scanf("%s%d", policy, &N);
+    key_t key = 10023; // Just a casual number
+    int shmid = shmget(key, N * sizeof(Process), IPC_CREAT | 0666);
+    proc = shmat(shmid, NULL, 0);
+
+    // Input
+    for(int i = 0; i < N; i++){
+        scanf("%s%d%d", proc[i].name, &proc[i].R, &proc[i].T);
+        proc[i].turn = proc[i].cnt = proc[i].robin = proc[i].running = 0;
+        proc[i].first = 1;
+    }
+
+    qsort(proc, N, sizeof(Process), compare);
+    int cnt = 0; // Time unit.
+    
+    // Affinity (1) (for child) (line 172)
+    CPU_ZERO(&mask);
+    CPU_SET(1, &mask);
+    
+    for(int i = 0; i < N; i++){
+        // Ready to run.
+        while(cnt++ < proc[i].R) Unit();
+        arrived++; // A process has arrived.
+        
+        int j; // Iterator
+        int remain, preempted = -1; // PSJF
+        switch(policy[0]){
+            // FIFO & RR & SJF
+            case 'F': case 'R': case 'S':
+                for(j = 0; j <= arrived; j++){
+                    if(j == arrived) proc[j].turn = 1;
+                    if(proc[j].turn == 1) break;
+                }
+                break;
+            
+            // PSJF
+            case 'P':
+                for(j = 0; j <= arrived; j++){
+                    if(j == arrived) proc[j].turn = 1;
+                    remain = proc[j].T - proc[j].cnt;
+                    if(remain > 0 && remain <= proc[arrived].T) break;
+                    if(proc[j].turn == 1) preempted = j;
+                }
+                
+                if(preempted != -1){
+                    // Preempt
+                    proc[preempted].running = 0;
+                    kill(proc[preempted].pids, SIGUSR3); 
+                }
+                break;
+        }
+
+        pid_t tmp = fork();        
+        if(tmp == 0){
+            sched_setaffinity(0, sizeof(mask), &mask); // Affinity
+            
+            struct timespec start_time, end_time;
+            // Running
+            while(1){
+                // A paused process would not be context switched.
+                // => No need to call sched_setscheduler().
+                if(proc[arrived].turn == 0) pause(); // Wait to be waken.
+                proc[arrived].running = 1; // Make sure to be waken up.
+                
+                int now = proc[arrived].cnt; // RR
+
+                if(proc[arrived].first == 1){
+                    proc[arrived].first = 0;
+                    clock_gettime(CLOCK_REALTIME, &start_time); // Get start_time (1st executed).
+                }
+
+                switch(policy[0]){
+                    // FIFO & SJF && PSJF
+                    case 'F': case 'S': case 'P':
+                        while(proc[arrived].cnt++ < proc[arrived].T 
+                              && proc[arrived].turn == 1) Unit();
+                        break;
+                    
+                    // RR
+                    case 'R':
+                        while(proc[arrived].cnt++ < proc[arrived].T
+                              && proc[arrived].cnt <= now + 500) Unit();
+                        if(proc[arrived].cnt > now + 500){
+                            proc[arrived].turn = 0;
+                            proc[arrived].robin = 1;
+                            proc[arrived].running = 0;
+                            kill(getppid(), SIGUSR1);
+                        }
+                        break;
+                }
+
+                if(proc[arrived].cnt >= proc[arrived].T) break; // Burst finished.
+            }
+           
+            // End of a child.
+            proc[arrived].turn = 0;
+            fprintf(stdout, "%s %d\n", proc[arrived].name, proc[arrived].pids);
+            fflush(stdout);
+            clock_gettime(CLOCK_REALTIME, &end_time); // Get end_time.
+            PRINT_TO_DMESG(start_time, end_time);
+            kill(getppid(), SIGUSR1); // Run another process.
+            exit(0);    
+        }else proc[arrived].pids = tmp;
+    }
+    
+    for(int i = 0; i < N; i++) wait(NULL);
+
+    // Delete shared memory.
+    shmdt(proc);
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
