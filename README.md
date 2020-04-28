f1c8db60676065b0f13dd7157b36231743ba31c7
HowardTang <b06902020@ntu.edu.tw>
2020-04-28 18:12:21 +0800

    Ubuntu18.04

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 - Spec: https://hackmd.io/@Ue96nvjESj2XsDXw532-qA/ryYqceUrU
 
 ## Environment
-- OS: Ubuntu 16.04
-- GCC version: 5.4.0
-- Python version: 3.5.2 (For running all input files, not necessary.)
+- OS: Ubuntu 18.04
+- GCC version: 7.5.0
+- Python version: 3.6.9 (For running all input files, not necessary.)
 
 ## Folders
 - compare: Theoretical time units vs. Practical time units
@@ -27,9 +27,20 @@ sudo ./make < <INPUT FILE>
 
 ## All input files:
 sudo python3 run_all_input.py <INPUT FILE DIRECTORY> <OUTPUT FILE DIRECTORY> <MAIN>
+ex: sudo python3 run_all_input.py ../OS_PJ1_Test ../output ../main
 
 ## Compare theoretical & practical time:
 python3 time_compare.py <THEO_UNIT FILE> <OUTPUT FILE DIRECTORY> <COMPARED FILE DIRECTORY>
+ex: sudo python3 time_compare.py Theo_unit.txt ../output ../compare
+```
+
+## Results
+```powershell
+Time per unit: 0.00146 s
+Difference between theoretical & practical time:
+- Max: 3.21697%
+- Min: 0.00959%
+- Avg: 0.49646%
 ```
 
 ## Remarks
--- python_files/time_compare.py
+++ python_files/time_compare.py
@@ -24,6 +24,9 @@ Unit = sum(Unit) / len(Unit) / 500
 cnt = 1
 max_diff = 0
 min_diff = 1e20
+tot_diff = 0
+tot_cnt = 0
+
 for f in sorted(os.listdir(output)):
     if f[:16] == 'TIME_MEASUREMENT' or f[-10:] == 'stdout.txt':
         continue
@@ -49,32 +52,37 @@ for f in sorted(os.listdir(output)):
                 BASE_T = 1e20
                 for ol in olines:
                     BASE = min(BASE, float(ol.split()[2]))
-                    BASE_T = min(BASE_T, round(Unit * float(Theo_unit[ol.split()[1]][1]), 4))
+                    BASE_T = min(BASE_T, round(Unit * float(Theo_unit[ol.split()[1]][1]), 5))
                 
                 for ol in olines:
                     ol = ol.split()
                     pid = ol[1]
-                    prac_start = str(round(float(ol[2]) - BASE, 4))
-                    prac_end = str(round(float(ol[3]) - BASE, 4))
-                    PRAC = str(round(float(prac_end) - float(prac_start), 4))
+                    prac_start = str(round(float(ol[2]) - BASE, 5))
+                    prac_end = str(round(float(ol[3]) - BASE, 5))
+                    PRAC = str(round(float(prac_end) - float(prac_start), 5))
                     prac_start += '0' * (8 - len(prac_start))
                     prac_end += '0' * (8 - len(prac_end))
                     PRAC += '0' * (8 - len(PRAC))
 
-                    theo_start = str(round(Unit * float(Theo_unit[pid][1]) - BASE_T, 4))
-                    theo_end = str(round(Unit * float(Theo_unit[pid][2]) - BASE_T, 4))
-                    THEO = str(round(float(theo_end) - float(theo_start), 4))
+                    theo_start = str(round(Unit * float(Theo_unit[pid][1]) - BASE_T, 5))
+                    theo_end = str(round(Unit * float(Theo_unit[pid][2]) - BASE_T, 5))
+                    THEO = str(round(float(theo_end) - float(theo_start), 5))
                     theo_start += '0' * (8 - len(theo_start))
                     theo_end += '0' * (8 - len(theo_end))
                     THEO += '0' * (8 - len(THEO))
 
-                    max_diff = max(max_diff, abs(float(PRAC) - float(THEO)) / float(THEO) * 100)
-                    min_diff = min(min_diff, abs(float(PRAC) - float(THEO)) / float(THEO) * 100)
+                    diff = abs(float(PRAC) - float(THEO)) / float(THEO) * 100
+                    tot_diff += diff
+                    tot_cnt += 1
+                    max_diff = max(max_diff, diff)
+                    min_diff = min(min_diff, diff) 
                     
                     cpf.write('|'.join([Theo_unit[pid][0], theo_start, theo_end, THEO, prac_start, prac_end, PRAC]) + '\n')
     
     cnt += 1
 
-print("Max_diff:", str(round(max_diff, 4)) + '%')
-print("Min_diff:", str(round(min_diff, 4)) + '%')
+print("Unit:", round(Unit, 5))
+print("Max_diff:", str(round(max_diff, 5)) + '%')
+print("Min_diff:", str(round(min_diff, 5)) + '%')
+print("Avg_diff:", str(round(tot_diff / tot_cnt, 5)) + '%')
 
